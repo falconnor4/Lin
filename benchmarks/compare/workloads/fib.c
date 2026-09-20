5480afb3f5fcab56e6b7cc48e837ef7bd839b6c8
Connor Brown <falconnor4@gmail.com>
2026-09-19 18:56:27 -0700

    Cleanup

--- benchmarks/compare/workloads/fib.c
+++
@@ -1,4 +0,0 @@
-/* The same program: naive Fibonacci. */
-#include <stdio.h>
-static long fib(long n) { return n <= 1 ? n : fib(n - 1) + fib(n - 2); }
-int main(void) { printf("%ld\n", fib(14)); return 0; }
